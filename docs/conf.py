c21d2356059c3dd61baaab4f07d2c8c6939563a9
Chris McDonough <chrism@plope.com>
2009-01-08 18:50:15 +0000

    Prep for 0.3.5.

--- docs/conf.py
+++ docs/conf.py
@@ -57,9 +57,9 @@ copyright = '2008, Repoze Developers <repoze-dev@lists.repoze.org>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.3.4'
+version = '0.3.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.3.4'
+release = '0.3.5'
 
 # There are two options for replacing |today|: either, you set today to
 # some non-false value, then it is used:
