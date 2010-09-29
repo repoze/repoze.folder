aa05c7963f1f4fc2f60084b742f4577d2b722566
Tres Seaver <tseaver@palladion.com>
2010-09-29 19:32:50 +0000

    Normalize docstrings.

--- repoze/folder/__init__.py
+++ repoze/folder/__init__.py
@@ -20,10 +20,9 @@ from BTrees.Length import Length
 sysencoding = sys.getdefaultencoding()
 
 class Folder(Persistent):
-    """
-    A folder implementation which acts much like a Python dictionary.
-    The keys are Unicode strings; the values are arbitrary Python
-    objects.
+    """ A folder implementation which acts much like a Python dictionary.
+
+    keys are Unicode strings; values are arbitrary Python objects.
     """
 
     # _num_objects=None below is b/w compat for older instances of
@@ -43,18 +42,24 @@ class Folder(Persistent):
         self._num_objects = Length(len(data))
 
     def keys(self):
-        """
-        Returns an iterable object containing the names associated
-        with the objects that appear in the folder.  Each name is
-        guaranteed to be a Unicode string.
+        """ See IFolder.
         """
         return self.data.keys()
 
     __iter__ = keys
 
-    def __len__(self):
+    def values(self):
+        """ See IFolder.
         """
-        Returns the number of objects in the container.
+        return self.data.values()
+
+    def items(self):
+        """ See IFolder.
+        """
+        return self.data.items()
+
+    def __len__(self):
+        """ See IFolder.
         """
         if self._num_objects is None:
             # can be arbitrarily expensive
@@ -62,94 +67,35 @@ class Folder(Persistent):
         return self._num_objects()
 
     def __nonzero__(self):
-        """ This is defined so code that does 'if folder:' will avoid
-        calling __len__ (which can be arbitrarily expensive). """
+        """ See IFolder.
+        """
         return True
 
-    def values(self):
-        """
-        Return a sequence-like object containing the objects that
-        appear in the folder.
-        """
-        return self.data.values()
-
-    def items(self):
-        """
-        Return a sequence-like object containing tuples of the form
-        (name, object) for the objects that appear in the folder.
-        Each name is guaranteed to be a Unicode string.
-        """
-        return self.data.items()
-
     def __getitem__(self, name):
-        """
-        Return the named object, or raise ``KeyError`` if the object
-        is not found.
-
+        """ See IFolder.
         """
         name = unicodify(name)
         return self.data[name]
 
     def get(self, name, default=None):
-        """
-        Return the named object, or the value of the `default`
-        argument if the object is not found.  Return the named object,
-        or raise ``KeyError`` if the object is not found.
-
-        `name`` will optimally be a Unicode string, because keys used
-        to look up objects are stored as Unicode.  However, if
-        ``name`` is a byte string, the system attempts to decode it to
-        Unicode using the default system encoding; if it cannot be
-        decoded using the system encoding, the system attempts to
-        decode it using the UTF-8 codec; and if that fails, a
-        TypeError is raised.
+        """ See IFolder.
         """
         name = unicodify(name)
         return self.data.get(name, default)
 
     def __contains__(self, name):
-        """
-        Return true if the named object appears in the folder.
-
-        `name`` will optimally be a Unicode string, because keys used
-        to look up objects are stored as Unicode.  However, if
-        ``name`` is a byte string, the system attempts to decode it to
-        Unicode using the default system encoding; if it cannot be
-        decoded using the system encoding, the system attempts to
-        decode it using the UTF-8 codec; and if that fails, a
-        TypeError is raised.
+        """ See IFolder.
         """
         name = unicodify(name)
         return self.data.has_key(name)
 
     def __setitem__(self, name, other):
-        """
-        Add the given object to the folder under the given name.  The
-        name must not be the empty string nor may it be of a type other
-        than ``basestring`` (a TypeError is raised if either case is true).
-
-        `name`` will optimally be a Unicode string, because keys used
-        to look up objects are stored as Unicode.  However, if
-        ``name`` is a byte string, the system attempts to decode it to
-        Unicode using the default system encoding; if it cannot be
-        decoded using the system encoding, the system attempts to
-        decode it using the UTF-8 codec; and if that fails, a
-        TypeError is raised.
-
-        This method sends a ``ObjectWillBeAddedEvent`` before the
-        object is set into the folder, and an ``ObjectAddedEvent``
-        after the object has been set into the folder.
+        """ See IFolder.
         """
         return self.add(name, other)
 
     def add(self, name, other, send_events=True):
-        """ Does the same thing as ``__delitem__`` but provides the
-        ``send_events`` argument to allow a user to suppress the
-        sending of folder events.  By default, the ``send_events``
-        flag is ``True``, meaning this method behaves exactly like
-        ``__setitem__`` (events are sent).  If ``send_events`` is
-        ``False`` or any other value that evaluates to false, folder
-        events will not be sent.
+        """ See IFolder.
         """
         if not isinstance(name, basestring):
             raise TypeError("Name must be a string rather than a %s" %
@@ -178,32 +124,12 @@ class Folder(Persistent):
             objectEventNotify(ObjectAddedEvent(other, self, name))
 
     def __delitem__(self, name):
-        """
-        Delete the named object from the folder. Raises a KeyError if
-        the object is not found.
-
-        `name`` will optimally be a Unicode string, because keys used
-        to look up objects are stored as Unicode.  However, if
-        ``name`` is a byte string, the system attempts to decode it to
-        Unicode using the default system encoding; if it cannot be
-        decoded using the system encoding, the system attempts to
-        decode it using the UTF-8 codec; and if that fails, a
-        TypeError is raised.
-
-        This method sends a ``ObjectWillBeRemovedEvent`` before the
-        object is set into the folder, and an ``ObjectRemovedEvent``
-        after the object has been set into the folder.
+        """ See IFolder.
         """
         return self.remove(name)
 
     def remove(self, name, send_events=True):
-        """ Does the same thing as ``__delitem__`` but provides the
-        ``send_events`` argument to allow a user to suppress the
-        sending of folder events.  By default, the ``send_events``
-        flag is ``True``, meaning this method behaves exactly like
-        ``__delitem__`` (events are sent).  If ``send_events`` is
-        ``False`` or any other value that evaluates to false, folder
-        events will not be sent. The removed object is returned.
+        """ See IFolder.
         """
         name = unicodify(name)
         other = self.data[name]
@@ -229,6 +155,8 @@ class Folder(Persistent):
         return other
 
     def pop(self, name, default=marker):
+        """ See IFolder.
+        """
         try:
             result = self.remove(name)
         except KeyError:
