a261a77e70e77cb2637e657434c09d111b605f72
Tres Seaver <tseaver@palladion.com>
2014-12-28 16:23:35 -0500

    Normalize static asset location.

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ html_style = 'repoze.css'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-html_logo = '.static/logo_hi.gif'
+html_logo = '_static/logo_hi.png'
 
 # The name of an image file (within the static path) to use as favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or
@@ -122,7 +122,7 @@ html_logo = '.static/logo_hi.gif'
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
@@ -183,7 +183,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the
 # top of the title page.
-latex_logo = '.static/logo_hi.gif'
+latex_logo = '_static/logo_hi.png'
 
 # For "manual" documents, if this is true, then toplevel headings are
 # parts, not chapters.
